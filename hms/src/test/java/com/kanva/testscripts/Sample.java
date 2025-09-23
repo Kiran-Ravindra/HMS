01ee2ecd421266c0392966c601b70bda044749b8
Kiran Ravindra <kiran@gmail.com>
2025-09-23 08:56:09 +0530

    Initial Commit

---
+++ hms/src/test/java/com/kanva/testscripts/Sample.java
@@ -0,0 +1,16 @@
+package com.kanva.testscripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Sample {
+
+	@Test
+	public void openBrowser() {
+
+
+		Reporter.log("Test Output",true);
+
+	}
+
+}
