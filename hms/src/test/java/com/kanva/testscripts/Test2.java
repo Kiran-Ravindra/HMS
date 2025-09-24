5706a3460121677afa0a5522390dbcecb9494c09
Kiran Ravindra <kiran@gmail.com>
2025-09-24 11:24:29 +0530

    Added Test2class

---
+++ hms/src/test/java/com/kanva/testscripts/Test2.java
@@ -0,0 +1,13 @@
+package com.kanva.testscripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+
+public class Test2 {
+    
+	@Test
+	public void closeBrowser() {
+		Reporter.log("Close Browser",true);
+	}
+}
